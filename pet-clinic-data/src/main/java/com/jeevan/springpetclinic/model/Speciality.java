13f5ca402f017aa51f7cf62b0f692c2978862a5c
jeevanraj61 <jeevanraj61@gmail.com>
2018-11-03 09:44:05 +0100

    Added properties to owner

---
+++ pet-clinic-data/src/main/java/com/jeevan/springpetclinic/model/Speciality.java
@@ -0,0 +1,14 @@
+package com.jeevan.springpetclinic.model;
+
+public class Speciality extends  BaseEntity{
+
+    private String desccription;
+
+    public String getDesccription() {
+        return desccription;
+    }
+
+    public void setDesccription(String desccription) {
+        this.desccription = desccription;
+    }
+}
