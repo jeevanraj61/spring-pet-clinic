0aaabced14c24e5184a61d102e76fbfe160a4a9c
jeevanraj61 <jeevanraj61@gmail.com>
2018-11-07 18:52:07 +0100

    Adding projet Lombok

--- pet-clinic-data/src/main/java/com/jeevan/springpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/jeevan/springpetclinic/model/Pet.java
@@ -1,12 +1,17 @@
 package com.jeevan.springpetclinic.model;
 
+import lombok.*;
 import org.hibernate.annotations.Cascade;
 import org.hibernate.annotations.JoinColumnOrFormula;
 
 import javax.persistence.*;
 import java.time.LocalDate;
 import java.util.Set;
-
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "pets")
 public class Pet extends BaseEntity{
@@ -27,44 +32,4 @@ public class Pet extends BaseEntity{
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
     private Set<Visit> visits;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public PetType getPetType() {
-        return petType;
-    }
-
-    public void setPetType(PetType petType) {
-        this.petType = petType;
-    }
-
-    public Owner getOwner() {
-        return owner;
-    }
-
-    public void setOwner(Owner owner) {
-        this.owner = owner;
-    }
-
-    public LocalDate getBirthDate() {
-        return birthDate;
-    }
-
-    public void setBirthDate(LocalDate birthDate) {
-        this.birthDate = birthDate;
-    }
-
-    public Set<Visit> getVisits() {
-        return visits;
-    }
-
-    public void setVisits(Set<Visit> visits) {
-        this.visits = visits;
-    }
 }
